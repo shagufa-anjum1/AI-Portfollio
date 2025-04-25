8c7426143c6411d12673afc72574549a64d08286
shagufa-anjum1 <shagufa.anjum2021@vitbhopal.ac.in>
2025-04-26 02:15:41 +0530

    landing Page FE UI

--- app/page.js
+++ app/page.js
@@ -15,18 +15,18 @@ export default function Home() {
     <main>
       <Image
         layout="fill"
-        src="/rs-big.webp"
-        alt="RoadsideCoder Banner"
+        src="/Image-Portfolio.png"
+        alt="Shagufa Anjum"
         objectFit="cover"
       />
       <div className="absolute px-4 w-full h-screen flex flex-col gap-5 items-center bottom-5">
         <h1 className="text-4xl font-Kanit md:text-5xl font-bold text-white mt-10">
-          Piyush Agarwal&rsquo;s AI Portfolio
+          Shagufa Anjum&rsquo;s AI Portfolio
         </h1>
 
         <section className="w-full flex-1 flex flex-col overflow-y-scroll">
           {noMessages ? (
-            <p className="text-center text-xl">Ask me Anything</p>
+            <p className="text-center text-3xl text-white font-extrabold">Ask me Anything</p>
           ) : (
             <>
               {messages.map((message, index) => {
@@ -39,7 +39,7 @@ export default function Home() {
                     } m-2 p-2 px-4 w-[70%] md:w-[80%] mt-4 text-gray-200`}
                     key={`message-${index}`}
                   >
-                    <b>{message.role === "user" ? "User:" : "Piyush:"}</b>{" "}
+                    <b>{message.role === "user" ? "User:" : "Shagufa:"}</b>{" "}
                     {message.content}
                   </div>
                 );
@@ -62,11 +62,11 @@ export default function Home() {
             value={input}
             type="text"
             placeholder="What's your hometown?"
-            className="py-3 px-5 flex-1 rounded text-black text-2xl border-2 border-gray-50 focus:outline-none focus:border-blue-500"
+            className="py-3 px-5 flex-1 rounded text-white text-2xl border-2 border-gray-50 focus:outline-none focus:border-purple-500"
           />
           <button
             type="submit"
-            className="bg-blue-500 hover:bg-blue-600 text-white rounded text-xl  px-5 cursor-pointer focus:outline-none disabled:bg-blue-400"
+            className="bg-purple-700 hover:bg-purple-800 text-white rounded text-xl  px-5 cursor-pointer focus:outline-none disabled:bg-purple-600"
           >
             Submit
           </button>
